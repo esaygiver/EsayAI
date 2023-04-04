f2ab0b8c8ab56f3cb483111ca4ec43534658609b
Emirhan Saygiver <saygivere@gmail.com>
2023-04-04 18:28:44 +0300

    HistoryView added

--- iOS-EsayAI/ContentView.swift
+++ iOS-EsayAI/ContentView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isPresented = false
+    
     var body: some View {
-        MainView()
+        NavigationStack {
+            MainView()
+                .sheet(isPresented: $isPresented) {
+                    NavigationStack {
+                        HistoryView()
+                            .navigationTitle("History")
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            isPresented = true
+                        } label: {
+                            Text("History")
+                        }
+                    }
+                }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(Model())
+            .environment(\.managedObjectContext, CoreDataManager.shared.persistentContainer.viewContext)
     }
 }
