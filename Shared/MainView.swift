7e72f10345f0821d359c40959d2031657f787ab5
Emirhan Saygiver <saygivere@gmail.com>
2023-04-04 16:31:49 +0300

    OpenAISwift imported

--- Shared/MainView.swift
+++ Shared/MainView.swift
@@ -6,26 +6,44 @@
 //
 
 import SwiftUI
+import OpenAISwift
 
 struct MainView: View {
     
     @State private var chatText = ""
+    @State private var answers = [String]()
+    
+    private let openAI = OpenAISwift(authToken: "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")
     
     private var isValidText: Bool {
         !chatText.isEmptyOrWhiteSpace
     }
     
     private func performSearch() {
-        
+        openAI.sendCompletion(with: chatText, maxTokens: 500) { result in
+            switch result {
+            case .success(let success):
+                if let answer = success.choices?.first?.text.trimmingCharacters(in: .whitespacesAndNewlines) {
+                    answers.append(answer)
+                }
+                
+            case .failure(let failure):
+                print(failure)
+            }
+        }
     }
     
     var body: some View {
         VStack {
+            List(answers, id: \.self) { answer in
+                Text(answer)
+            }
             Spacer()
             HStack {
                 TextField("Ask ChatGPT...", text: $chatText)
                     .textFieldStyle(.roundedBorder)
                     .border(.brown)
+                    .autocorrectionDisabled()
                 
                 Button {
                     performSearch()
