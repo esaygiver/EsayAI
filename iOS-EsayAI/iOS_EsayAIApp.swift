16afdbd604dee09a199813f178b5d1bfa8a6c0a1
Emirhan Saygiver <saygivere@gmail.com>
2023-04-04 15:27:42 +0300

    Initial Commit

---
+++ iOS-EsayAI/iOS_EsayAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_EsayAIApp.swift
+//  iOS-EsayAI
+//
+//  Created by Emirhan Saygiver on 4.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_EsayAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macOS-EsayAI/macOS_EsayAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_EsayAIApp.swift
+//  macOS-EsayAI
+//
+//  Created by Emirhan Saygiver on 4.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct macOS_EsayAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
